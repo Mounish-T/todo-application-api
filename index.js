96349475dbe09ea11f530d6bc35b2791d69c5150
Mounish-T <tmounish02@gmail.com>
2025-07-17 22:36:34 +0530

    Host server on the current connected wifi address

--- index.js
+++ index.js
@@ -2,10 +2,27 @@ import express from 'express';
 import cors from 'cors';
 import tasksRoutes from './routes/tasks.routes.js';
 import connectDB from './lib/db.js';
+import os from 'os';
 
 const app = express();
 const PORT = 1000; // Set the Port where the Server is Listening at
-const customIp = '192.168.50.85'; // Set the custom Ip address to run the server
+// const customIp = '0.0.0.0'; // Set the wifi Ip address to run the server
+
+function getLocalIP() {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const net of interfaces[name]) {
+      // Skip over non-IPv4 and internal addresses (like 127.0.0.1)
+      if (net.family === 'IPv4' && !net.internal) {
+        return net.address;
+      }
+    }
+  }
+  return 'IP not found';
+}
+
+const customIp = getLocalIP();
+
 
 app.use(cors());
 app.use(express.json());
@@ -14,6 +31,7 @@ app.use(express.urlencoded({extended: true}));
 // Connect the database
 connectDB();
 
+// 0.0.0.0 => open access -> By default, it run in the current wifi network
 app.listen(PORT, customIp, ()=>{
     console.log(`The Server is running at http://${customIp}:${PORT}`);
 })
